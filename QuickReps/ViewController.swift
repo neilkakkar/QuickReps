70bac2f77e99898c9239b434e92fc78064a39f83
Neil Kakkar <nkakkar@bloomberg.net>
2019-09-01 15:15:21 +0100

    Initial Commit

---
+++ QuickReps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickReps
+//
+//  Created by nkakkar on 01/09/2019.
+//  Copyright © 2019 neilkakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QuickRepsTests/QuickRepsTests.swift
@@ -0,0 +1,34 @@
+//
+//  QuickRepsTests.swift
+//  QuickRepsTests
+//
+//  Created by nkakkar on 01/09/2019.
+//  Copyright © 2019 neilkakkar. All rights reserved.
+//
+
+import XCTest
+@testable import QuickReps
+
+class QuickRepsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
